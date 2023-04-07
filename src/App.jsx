9908fe6ba91d774d2e3d8b6e534e9b3b22353c62
Vatangame Yazılım <gurkanatik35@gmail.com>
2023-04-07 13:43:17 +0300

    load more completed

--- src/App.jsx
+++ src/App.jsx
@@ -11,19 +11,11 @@ function App() {
     const [filters, setFilters] = useState([])
     const [selectedFilters, setSelectedFilters] = useState({})
     const [searchValue, setSearchValue] = useState('')
+    const [page, setPage] = useState(1)
+    const [perItem] = useState(12)
 
-    const filteredChampions = champions.filter((champion) => {
-        return Object.entries(selectedFilters).every(([filterKey, filterValues]) => {
-            return filterValues.every((filterValue) => {
-                return champion[filterKey].includes(filterValue)
-            })
-        })
-    }).filter((champion) => {
-        if (searchValue.length > 0) {
-            return champion.name.toLowerCase().includes(searchValue.toLowerCase())
-        }
-        return champion
-    })
+    const filteredChampions = getFilteredChampions()
+    const listChampions = paginateChampions()
 
     useEffect(() => {
         setTags(getTags)
@@ -36,6 +28,25 @@ function App() {
             {caption: "partype", values: resources, type: "radio"},
         ])
     }, [tags, resources])
+
+    function getFilteredChampions() {
+        return champions.filter((champion) => {
+            return Object.entries(selectedFilters).every(([filterKey, filterValues]) => {
+                return filterValues.every((filterValue) => {
+                    return champion[filterKey].includes(filterValue)
+                })
+            })
+        }).filter((champion) => {
+            if (searchValue.length > 0) {
+                return champion.name.toLowerCase().includes(searchValue.toLowerCase())
+            }
+            return champion
+        })
+    }
+
+    function paginateChampions() {
+        return getFilteredChampions().splice(0, page * perItem)
+    }
 
     function getTags() {
         let tags = []
@@ -55,14 +66,22 @@ function App() {
 
     return (
         <>
-            <Search setSearchValue={setSearchValue} />
+            <Search setSearchValue={setSearchValue}/>
             <div className="flex flex-wrap lg:flex-nowrap px-2 lg:px-20 mt-4 mx-0 md:mx-m6">
                 <div className="w-full md:w-3/12 lg:w-4/12 px-6">
-                    <Filters filters={filters} champions={champions} selectedFilters={selectedFilters} setSelectedFilters={setSelectedFilters} />
+                    <Filters filters={filters} champions={champions} selectedFilters={selectedFilters}
+                             setSelectedFilters={setSelectedFilters}/>
                 </div>
                 <div className="w-full md:w-9/12 lg:w-8/12 px-6">
-                    <Champions champions={filteredChampions}/>
-                    {filteredChampions.length === 0 && (<div className="p-4 bg-yellow-400 text-gray-700">Champion not found</div>)}
+                    <Champions champions={listChampions}/>
+                    {(listChampions.length === 0) && (
+                        <div className="p-4 bg-yellow-400 text-gray-700">Champion not found</div>)}
+                    {(filteredChampions.length !== 0 && filteredChampions.length !== listChampions.length) && (
+                        <button
+                            onClick={() => setPage(page + 1)}
+                            className="px-4 py-2 bg-gray-200 text-black table mx-auto my-4"
+                        >Load More</button>
+                    )}
                 </div>
             </div>
         </>
